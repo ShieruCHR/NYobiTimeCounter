d3e65c48004f847bedbd98a0b9f7b540eaadd38f
Shieru <81741561+ShieruCHR@users.noreply.github.com>
2022-08-22 17:56:14 +0900

    視聴済みと未視聴の合計も表示するよう変更

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@ window.onclick = apply;
 window.onload = apply;
 
 function apply() {
-    let total = calculate();
-    let totalString = "動画時間の合計: " + total[0] + "時間" + total[1] + "分" + total[2] + "秒";
+    let total = calculateTotal();
+    let nonWatched = calculateNonWatched();
+    let watched = calculateWatched();
+    let totalString = "動画時間の合計: " + formatTime(total) + "\n視聴済み: " + formatTime(watched) + "\n未視聴: " + formatTime(nonWatched);
     let element = document.getElementById("total-time");
     if (element == null) {
         element = document.createElement("p");
@@ -15,14 +17,53 @@ function apply() {
     element.innerText = totalString;
 }
 
+function formatTime(time) {
+    return time[0] + "時間" + time[1] + "分" + time[2] + "秒";
+}
+
+function getAllTimeElements() {
+    return Array.from(document.getElementsByClassName("movie-length"));
+}
+
 function calculate() {
-    let timeElements = Array.from(document.getElementsByClassName("movie-length"));
+    return sumAllTimes(getAllTimes(getAllTimeElements()));
+}
+
+function calculateTotal() {
+    return sumAllTimes(getAllTimes(getAllTimeElements()));
+}
+
+function calculateNonWatched() {
+    let allElements = getAllTimeElements();
+    let nonWatchedElements = allElements.filter(element => {
+        let card = element.parentElement.parentElement.parentElement;
+        return !card.classList.contains("good") && !card.classList.contains("bad");
+    });
+    return sumAllTimes(getAllTimes(nonWatchedElements));
+}
+
+function calculateWatched() {
+    let allElements = getAllTimeElements();
+    let watchedElements = allElements.filter(element => {
+        let card = element.parentElement.parentElement.parentElement;
+        return card.classList.contains("good") || card.classList.contains("bad");
+    });
+    return sumAllTimes(getAllTimes(watchedElements));
+}
+
+function getAllTimes(timeElements) {
     let times = [];
     timeElements.forEach(element => {
         let splitted = element.innerText.split(":");
-        times.push([parseInt(splitted[0]), parseInt(splitted[1])]);
+        times.push([
+            parseInt(splitted[0]),
+            parseInt(splitted[1])
+        ]);
     });
+    return times;
+}
 
+function sumAllTimes(times) {
     let totalSeconds = 0;
     times.forEach(time => {
         totalSeconds += time[0] * 60 + time[1];
